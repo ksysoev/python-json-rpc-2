0f0d97049879f84ea6f941187adfd68ce18d7027
Kirill Sysoev <k.sysoev@me.com>
2019-05-04 12:10:18 +0700

    fixing linter error in server package

--- aiojsonrpc/server.py
+++ aiojsonrpc/server.py
@@ -1,17 +1,24 @@
+"""JSON RPC Server
+
+Package for work with rpc server
+"""
+
 from .router import Router
 from .request import RequestFactory
 from .response import ErrorResponse
-from .exceptions import MethodNotFound, InvalidRequest, ParserError
+from .exceptions import InvalidRequest, ParserError
 
 class Server():
-    def __init__(self, router:Router = Router, 
-                 request_factory:RequestFactory = RequestFactory,
+    """Class which represent JSON RPC server"""
+    def __init__(self, router: Router = Router,
+                 request_factory: RequestFactory = RequestFactory,
                  ):
 
         self.router = router
         self.request_factory = request_factory
-        
-    def dispatch(self, request_data:str):
+
+    def dispatch(self, request_data: str):
+        """Method for handling JSON RPC requests"""
         try:
             request = self.request_factory.make_request(request_data)
         except (InvalidRequest, ParserError) as error:
