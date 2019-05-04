0140ba3a25676470d9b56ef83f2671aec902af7a
Kirill Sysoev <k.sysoev@me.com>
2019-05-04 12:01:07 +0700

    fixing linter error in router package

--- aiojsonrpc/router.py
+++ aiojsonrpc/router.py
@@ -1,20 +1,30 @@
+"""Router
+
+Module for handling routing  request to methods
+"""
+
 from .exceptions import MethodNotFound
 
 class Router():
+    """Routing serves as storage for RPC methods. """
+
     _methods = {}
 
     @classmethod
     def method(cls, method):
         """Decorator for adding new method to Router"""
+
         if method.__name__ in cls._methods:
             raise ValueError('Dublicate Method Name')
-        
+
         cls._methods[method.__name__] = method
 
         return method
-    
+
     @classmethod
     def dispatch(cls, request):
+        """Method returns function for requered method"""
+
         if request.method not in cls._methods:
             raise MethodNotFound(f"Method {request.method} isn't found")
 
@@ -22,12 +32,6 @@ class Router():
 
     @classmethod
     def clear(cls):
+        """Method removes all methods from Router"""
+
         cls._methods.clear()
-
-        
-
-
-
-
-
-        
